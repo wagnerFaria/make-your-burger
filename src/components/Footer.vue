21a7ed5058a95d365c8a5f5a8427b61117370154
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-01-14 09:55:08 -0400

    Arrumando rotas, Navbar e Footer inicial

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="home"></div>
+  <div>teste</div>
 </template>
-
 <script>
 export default {
-  name: "Home",
-  components: {},
+  name: "Footer",
 };
 </script>
+<style scoped></style>
