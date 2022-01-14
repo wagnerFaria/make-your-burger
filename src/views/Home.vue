0a00c0826aba39bc7353fc11bb3c9ad6ded660b0
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-01-14 14:15:54 -0400

    Criando banner da aplicação

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu burger:</h1>
+    <p>formulário</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Banner,
+  },
 };
 </script>
