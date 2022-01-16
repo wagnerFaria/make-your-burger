55887ab0dc0228de7bab7bb90182fb1192b81f47
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-01-16 08:22:34 -0400

    Removendo pedidos do sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -40,7 +40,9 @@
                 {{ st.tipo }}
               </option>
             </select>
-            <button class="delete-btn">Cancelar</button>
+            <button class="delete-btn" @click.prevent="deleteBurger(burger.id)">
+              Cancelar
+            </button>
           </div>
         </div>
       </div>
@@ -68,6 +70,16 @@ export default {
       const req = await fetch("http://localhost:3000/status");
       const data = await req.json();
       this.status = data;
+    },
+    async deleteBurger(id) {
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE",
+      });
+      await req.json();
+
+      //mensagem
+
+      this.getPedidos();
     },
   },
   mounted() {
